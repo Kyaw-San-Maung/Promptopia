4f3b3fffe6834cf5a71800cb204fa7a817a8b66a
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-01-05 10:00:40 +0630

    Creating Navigation bar.

---
+++ components/Nav.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {signIn, signOut, useSession, getProvider} from "next-auth/react"
+
+const Nav = () => {
+  const isUserLogin = true;
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLogin ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="profile">
+              <Image
+                src="assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
